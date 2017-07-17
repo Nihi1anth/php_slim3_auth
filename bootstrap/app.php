0f3dd80ea08e1df75cdd594814a09e74dd4c9152
Nikolay <ishaltbecome@gmail.com>
2017-07-17 15:43:52 +0000

    second commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,4 +12,19 @@ $settings = [
 
 $app = new \Slim\App($settings);
 
+$container = $app->getContainer();
+
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
+        'cache' => false,
+    ]);
+    
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $c->router,
+        $c->request->getUri()
+    ));
+    
+    return $view;
+};
+
 require __DIR__ . '/../app/routes.php';
